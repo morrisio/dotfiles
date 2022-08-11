cc6aae50d3e3261c62326240c04e33504dd6f829
Pawel Pisarzewski <morris@morris.io>
2022-08-11 19:39:25 +0200

    Remove Rust and Python support

--- nvim-lua/lua/morrisio/cmp.lua
+++ nvim-lua/lua/morrisio/cmp.lua
@@ -79,33 +79,3 @@ require("lspconfig").gopls.setup(config({
         },
     },
 }))
-
--- Rust
-local rust_opts = {
-    tools = { -- rust-tools options
-        autoSetHints = true,
-        hover_with_actions = true,
-        inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-        },
-    },
-    server = {
-        settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            ["rust-analyzer"] = {
-                -- enable clippy on save
-                checkOnSave = {
-                    command = "clippy"
-                },
-            }
-        }
-    },
-}
-
-require('rust-tools').setup(rust_opts)
-
--- Python
-require("lspconfig").pyright.setup{}
