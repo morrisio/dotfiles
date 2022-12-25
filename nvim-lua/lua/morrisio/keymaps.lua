35761f88797d9123eda71c8d8fa64490bba1661a
Pawel Pisarzewski <morris@morris.io>
2022-12-25 21:58:08 +0100

    Move telescope config to separate file

--- nvim-lua/lua/morrisio/keymaps.lua
+++ nvim-lua/lua/morrisio/keymaps.lua
@@ -1,38 +1,33 @@
-local opts = { noremap = true, silent = true }
-local insert = "i"
-local normal = "n"
-local term = "t"
+-- Get rid of Escape
+vim.api.nvim_set_keymap("i", "fd", "<Esc>", {})	-- Don't reach for Esc
+vim.api.nvim_set_keymap("t", "fd", "<c-\\><c-n>", {}) -- ... including in the terminal
 
-vim.api.nvim_set_keymap(insert, "fd", "<Esc>", opts)	-- Don't reach for Esc
-vim.api.nvim_set_keymap(term, "fd", "<c-\\><c-n>", opts)-- ... including in terminal
+-- Leader
 vim.g.mapleader = " " 					                -- Use Space as the leader key
 
 -- Easier window navigation
-vim.api.nvim_set_keymap(normal, "<leader>j", "<c-w><c-j>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>k", "<c-w><c-k>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>h", "<c-w><c-h>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>l", "<c-w><c-l>", opts)
+vim.api.nvim_set_keymap("n", "<leader>j", "<c-w><c-j>", {})
+vim.api.nvim_set_keymap("n", "<leader>k", "<c-w><c-k>", {})
+vim.api.nvim_set_keymap("n", "<leader>h", "<c-w><c-h>", {})
+vim.api.nvim_set_keymap("n", "<leader>l", "<c-w><c-l>", {})
 
 -- Move in visual line
-vim.api.nvim_set_keymap(normal, "j", "gj", opts)
-vim.api.nvim_set_keymap(normal, "k", "gk", opts)
+vim.api.nvim_set_keymap("n", "j", "gj", {})
+vim.api.nvim_set_keymap("n", "k", "gk", {})
 
-vim.api.nvim_set_keymap(normal, "Y", "y$", opts)	-- Yank till end of line
+vim.api.nvim_set_keymap("n", "Y", "y$", {})	-- Yank till end of line
 
 -- Keep cursor in the center when going next/prev
-vim.api.nvim_set_keymap(normal, "n", "nzzzv", opts)
-vim.api.nvim_set_keymap(normal, "N", "Nzzzv", opts)
+vim.api.nvim_set_keymap("n", "n", "nzzzv", {})
+vim.api.nvim_set_keymap("n", "N", "Nzzzv", {})
 
 -- Undo on more characters
-vim.api.nvim_set_keymap(insert, ",", ",<c-g>u", opts)
-vim.api.nvim_set_keymap(insert, ".", ".<c-g>u", opts)
-vim.api.nvim_set_keymap(insert, "!", "!<c-g>u", opts)
-vim.api.nvim_set_keymap(insert, "?", "?<c-g>u", opts)
+vim.api.nvim_set_keymap("i", ",", ",<c-g>u", {})
+vim.api.nvim_set_keymap("i", ".", ".<c-g>u", {})
+vim.api.nvim_set_keymap("i", "!", "!<c-g>u", {})
+vim.api.nvim_set_keymap("i", "?", "?<c-g>u", {})
 
 -- File finding
-vim.api.nvim_set_keymap(normal, "<c-n>", ":NERDTreeToggle<CR>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>fg", "<cmd>Telescope live_grep<CR>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
-vim.api.nvim_set_keymap(normal, "<leader>ft", ":NERDTreeFind<CR>", opts)
+vim.api.nvim_set_keymap("n", "<c-n>", ":NERDTreeToggle<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>ft", ":NERDTreeFind<CR>", {})
+
