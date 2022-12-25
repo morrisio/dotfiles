6f5df1f3e7670ed1d3d0c03664fc3347924b20ba
Pawel Pisarzewski <morris@morris.io>
2022-12-25 21:58:45 +0100

    Remove obsolete colors

--- nvim-lua/lua/morrisio/packer.lua
+++ nvim-lua/lua/morrisio/packer.lua
@@ -1,46 +1,45 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 
-    -- Basics
+	-- Basics
 	use 'vim-airline/vim-airline'
 	use 'scrooloose/nerdtree'
 	use 'jiangmiao/auto-pairs'
 	use 'tpope/vim-fugitive'
-    use 'tpope/vim-surround'
+	use 'tpope/vim-surround'
 	use 'preservim/vim-pencil'
 
-    -- Color schemes
+	-- Color schemes
 	use 'morhetz/gruvbox'
+	use 'ajmwagar/vim-deus'
 	use 'KKPMW/distilled-vim'
 	use 'preservim/vim-colors-pencil'
-	use 'rainglow/vim'
-    use 'arcticicestudio/nord-vim'
-    use 'rafi/awesome-vim-colorschemes'
+	use 'arcticicestudio/nord-vim'
 
-    -- Navigators
+	-- Navigators
 	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 	use 'nvim-lua/plenary.nvim'
 	use 'nvim-telescope/telescope.nvim'
 	use 'BurntSushi/ripgrep'
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 	use 'nvim-treesitter/playground'
-    use {'ggandor/leap.nvim'}
+	use {'ggandor/leap.nvim'}
 
-    -- LSP/completion
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
-    use 'L3MON4D3/LuaSnip'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cpm-nvim-lsp-signature-help'
-    use 'hrsh7th/cmp-nvim-lsp-document-symbol'
+	-- LSP/completion
+	use 'neovim/nvim-lspconfig'
+	use 'williamboman/nvim-lsp-installer'
+	use 'L3MON4D3/LuaSnip'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cpm-nvim-lsp-signature-help'
+	use 'hrsh7th/cmp-nvim-lsp-document-symbol'
 
-    -- Golang
-    use 'fatih/vim-go'
+	-- Golang
+	use 'fatih/vim-go'
 
-    -- Rust
-    use 'simrat39/rust-tools.nvim'
+	-- Rust
+	use 'simrat39/rust-tools.nvim'
 end)
