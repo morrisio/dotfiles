be315dabb68c28b99b87691e7ee3df94d975065e
Pawel Pisarzewski <morris@morris.io>
2022-12-25 22:03:23 +0100

    Move Nerdtree config to separate file

--- nvim-lua/lua/morrisio/keymaps.lua
+++ nvim-lua/lua/morrisio/keymaps.lua
@@ -26,8 +26,3 @@ vim.api.nvim_set_keymap("i", ",", ",<c-g>u", {})
 vim.api.nvim_set_keymap("i", ".", ".<c-g>u", {})
 vim.api.nvim_set_keymap("i", "!", "!<c-g>u", {})
 vim.api.nvim_set_keymap("i", "?", "?<c-g>u", {})
-
--- File finding
-vim.api.nvim_set_keymap("n", "<c-n>", ":NERDTreeToggle<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>ft", ":NERDTreeFind<CR>", {})
-
---
+++ nvim-lua/lua/morrisio/nerdtree.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap("n", "<c-n>", ":NERDTreeToggle<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>ft", ":NERDTreeFind<CR>", {})
+
