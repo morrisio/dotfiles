faa4d80c206ba4c095e3ab00bc247766f342b47b
Pawel Pisarzewski <morris@morris.io>
2022-12-26 08:58:18 +0100

    Fix formatting

--- nvim-lua/lua/morrisio/treesitter.lua
+++ nvim-lua/lua/morrisio/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require "nvim-treesitter.configs".setup {
   -- A list of parser names, or "all"
   ensure_installed = { "help", "lua", "go", "rust" },
 
@@ -14,7 +14,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
