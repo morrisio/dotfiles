e89483ab195fda77ada88e6c3de729d85a9e5135
Pawel Pisarzewski <morris@morris.io>
2022-12-26 15:20:08 +0100

    Update keymaps and opts

--- nvim-lua/lua/morrisio/keymaps.lua
+++ nvim-lua/lua/morrisio/keymaps.lua
@@ -1,28 +1,32 @@
--- Get rid of Escape
-vim.api.nvim_set_keymap("i", "fd", "<Esc>", {})	-- Don't reach for Esc
-vim.api.nvim_set_keymap("t", "fd", "<c-\\><c-n>", {}) -- ... including in the terminal
-
 -- Leader
-vim.g.mapleader = " " 					                -- Use Space as the leader key
+vim.g.mapleader = " " 					        -- Use Space as the leader key
+
+-- Get rid of Escape
+vim.keymap.set("i", "fd", "<Esc>", {})	        -- Don't reach for Esc
+vim.keymap.set("t", "fd", "<c-\\><c-n>", {})    -- ... including in the terminal
+
+-- Move by visual line
+vim.keymap.set("n", "j", "gj", {})
+vim.keymap.set("n", "k", "gk", {})
 
 -- Easier window navigation
-vim.api.nvim_set_keymap("n", "<leader>j", "<c-w><c-j>", {})
-vim.api.nvim_set_keymap("n", "<leader>k", "<c-w><c-k>", {})
-vim.api.nvim_set_keymap("n", "<leader>h", "<c-w><c-h>", {})
-vim.api.nvim_set_keymap("n", "<leader>l", "<c-w><c-l>", {})
+vim.keymap.set("n", "<leader>j", "<c-w><c-j>", {})
+vim.keymap.set("n", "<leader>k", "<c-w><c-k>", {})
+vim.keymap.set("n", "<leader>h", "<c-w><c-h>", {})
+vim.keymap.set("n", "<leader>l", "<c-w><c-l>", {})
 
--- Move in visual line
-vim.api.nvim_set_keymap("n", "j", "gj", {})
-vim.api.nvim_set_keymap("n", "k", "gk", {})
+-- Move visual selection up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.api.nvim_set_keymap("n", "Y", "y$", {})	-- Yank till end of line
+vim.keymap.set("n", "Y", "y$", {})	-- Yank till end of line
 
 -- Keep cursor in the center when going next/prev
-vim.api.nvim_set_keymap("n", "n", "nzzzv", {})
-vim.api.nvim_set_keymap("n", "N", "Nzzzv", {})
+vim.keymap.set("n", "n", "nzzzv", {})
+vim.keymap.set("n", "N", "Nzzzv", {})
 
 -- Undo on more characters
-vim.api.nvim_set_keymap("i", ",", ",<c-g>u", {})
-vim.api.nvim_set_keymap("i", ".", ".<c-g>u", {})
-vim.api.nvim_set_keymap("i", "!", "!<c-g>u", {})
-vim.api.nvim_set_keymap("i", "?", "?<c-g>u", {})
+vim.keymap.set("i", ",", ",<c-g>u", {})
+vim.keymap.set("i", ".", ".<c-g>u", {})
+vim.keymap.set("i", "!", "!<c-g>u", {})
+vim.keymap.set("i", "?", "?<c-g>u", {})
--- nvim-lua/lua/morrisio/options.lua
+++ nvim-lua/lua/morrisio/options.lua
@@ -1,34 +1,48 @@
 local options = {
-	backup = false, 		-- Don't create a backup file
 	clipboard = "unnamedplus", 	-- Use system clipboard
 	cmdheight = 2,			-- Higher command line
 	completeopt = {             	-- Make complete menu behave like VS
 	"menuone", "preview", "longest" 
 },
 cursorline = true,		    	-- Highlight current line
-expandtab = true,		    	-- Tabs to spaces
 fileencoding = "utf-8",			-- Save as UTF-8
+updatetime = 100,		    	-- Faster completion
+wildmenu = true,			-- Enhanced commandline completion
+
 foldexpr = "nvim_treesitter#foldexpr()",		-- Use Treesitter for folding
 foldlevelstart = 20,        		-- Open files unfolded
 foldmethod = "expr",			-- Use Treesitter for folding
 hidden = true,			    	-- Abandoned buffers are hidden rather than deleted
+
 hlsearch = true,			-- Highlight all search matches
 incsearch = true,		    	-- Update search results after each character
-linebreak = true,		    	-- Softwrap lines at word boundaries
+
+colorcolumn = "80",
 number = true,              		-- Show line numbers
+relativenumber = true,          -- Show relative line numbers
+
 scrolloff = 8,              		-- Scroll this number of lines from top/bottom
-shiftwidth = 4,             		-- Indentation is 4 spaces deep
 showcmd = true,             		-- Show command at the bottom
 signcolumn = "yes",		    	-- Always show the sign column (for breakpoints, linters)
 ignorecase = true,		    	-- Ignore case when searching by default (but see below)
 smartcase = true,		    	-- Switch to case sensitive if search patter contains uppercase chars
 splitbelow = true,		    	-- Open splits below
 splitright = false,		    	-- Open splits to the left
+
+expandtab = true,		    	-- Tabs to spaces
+shiftwidth = 4,             	-- Indentation is 4 spaces deep
 tabstop = 4,			    	-- Tabs are 4 columns wide
+softtabstop = 4,
+
 termguicolors = true,
-updatetime = 300,		    	-- Faster completion
-wildmenu = true,			-- Enhanced commandline completion
+
 wrap = true,			    	-- Softwrap text
+linebreak = true,		    	-- Softwrap lines at word boundaries
+
+swapfile = false,                -- No swaps or backups
+backup = false,
+undodir = os.getenv("HOME") .. "/.vim/undodir",
+undofile = true,
 }
 
 for k, v in pairs(options) do
