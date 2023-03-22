ede7084cf9629105e5713710163a95bda99eac45
Pawel Pisarzewski <morris@morris.io>
2023-03-22 09:48:36 +0100

    Add mappings for git and register searching

--- nvim-lua/lua/morrisio/telescope.lua
+++ nvim-lua/lua/morrisio/telescope.lua
@@ -1,5 +1,10 @@
 local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fr", builtin.registers, {})
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>fx", builtin.diagnostics, {})
