6ac0fa76f4894d0650bf3f88c0e13cd5c3d0e8d5
Pawel Pisarzewski <morris@morris.io>
2024-11-28 11:55:02 +0100

    Show relative path in lualine

--- nvim/lua/custom/lualine.lua
+++ nvim/lua/custom/lualine.lua
@@ -1,10 +1,18 @@
-local lualine = require "lualine"
+local lualine = require("lualine")
 
-lualine.setup {
-    options = {
-        icons_enabled = true,
-        theme = "codedark",
-        section_separators = {},
-        component_separators = {},
-    }
-}
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "codedark",
+		section_separators = {},
+		component_separators = {},
+	},
+	sections = {
+		lualine_c = {
+			{
+				"filename",
+				path = 1,
+			},
+		},
+	},
+})
