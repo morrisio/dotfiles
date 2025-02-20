df6fc7fd0af0b5b2e91521d802e885851a543f58
Pawel Pisarzewski <morris@morris.io>
2025-02-20 12:21:41 +0100

    Add DAP config

--- nvim/lua/custom/nvim-dap.lua
+++ nvim/lua/custom/nvim-dap.lua
@@ -1,17 +1,34 @@
 local dap = require("dap")
+local dapui = require("dapui")
+local dapgo = require("dap-go")
 
-dap.adapters.godot = {
-	type = "server",
-	host = "127.0.0.1",
-	port = 6006,
-}
+dapui.setup()
+dapgo.setup()
 
-dap.configurations.gdscript = {
-	{
-		type = "godot",
-		request = "launch",
-		name = "Launch scene",
-		project = "${workspaceFolder}",
-		launch_scene = true,
-	},
-}
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>bb", ":lua require'dap'.toggle_breakpoint()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bl", ":lua require'dap'.list_breakpoints()<CR>:cw<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bx", ":lua require'dap'.clear_breakpoints()<CR>", {})
+
+vim.api.nvim_set_keymap("n", "<leader>bq", ":lua require'dap'.terminate()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>br", ":lua require'dap'.restart()<CR>", {})
+
+vim.api.nvim_set_keymap("n", "<leader>bi", ":lua require'dap'.step_into()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bp", ":lua require'dap'.step_out()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bo", ":lua require'dap'.step_over()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bn", ":lua require'dap'.continue()<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>bh", ":lua require'dap'.run_to_cursor()<CR>", {})
+
+vim.api.nvim_set_keymap("n", "<leader>bt", ":lua require'dap-go'.debug_test()<CR>", {})
+
+-- nnoremap \; :lua require'dap'.run_last()<CR>
+
+-- nnoremap \e :lua require("dapui").eval(nil, { enter = true })<CR>
+-- nnoremap \E :lua require'dap'.repl.open()<CR>
--- nvim/lua/custom/plugins/nvim-dap.lua
+++ nvim/lua/custom/plugins/nvim-dap.lua
@@ -1,6 +1,11 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"rcarriga/nvim-dap-ui",
+			"leoluz/nvim-dap-go",
+		},
 		config = function()
 			require("custom.nvim-dap")
 		end,
