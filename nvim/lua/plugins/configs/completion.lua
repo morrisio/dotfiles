4d96b4aa2c9e3c9a6547ef5f5c5bc73aa1b61351
Pawel Pisarzewski <morris@morris.io>
2025-11-11 16:56:23 +0100

    Add borders to completion window

--- nvim/lua/plugins/configs/completion.lua
+++ nvim/lua/plugins/configs/completion.lua
@@ -6,6 +6,10 @@ cmp.setup({
 		{ name = "path" },
 		{ name = "buffer" },
 	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
 	mapping = {
 		["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
 		["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -18,7 +22,7 @@ cmp.setup({
 		),
 	},
 
-	-- Enable luasnip to handle snippet expansion for nvim-cmp
+	-- Enable native snippets to handle snippet expansion for nvim-cmp
 	snippet = {
 		expand = function(args)
 			vim.snippet.expand(args.body)
