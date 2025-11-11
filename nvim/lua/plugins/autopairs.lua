eeb6b07ce8e3b10fd84bd751dc5516c13effd2c3
Pawel Pisarzewski <morris@morris.io>
2025-11-11 16:55:59 +0100

    Clean up dependencies

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,5 @@
 return {
 	"windwp/nvim-autopairs",
-	dependencies = { "hrsh7th/nvim-cmp" },
-	config = function()
-		require("nvim-autopairs").setup({})
-	end,
+	event = "InsertEnter",
+	config = true,
 }
