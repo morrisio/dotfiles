58cfb24529820baf7098be63503def44ca602804
Pawel Pisarzewski <morris@morris.io>
2025-11-15 23:13:01 +0100

    Fix Leap config

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,6 +1,9 @@
+local function config()
+	vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+	vim.keymap.set("n", "S", "<Plug>(leap-backward)")
+end
+
 return {
 	"ggandor/leap.nvim",
-	config = function()
-		require("plugins.configs.leap")
-	end,
+	config = config,
 }
